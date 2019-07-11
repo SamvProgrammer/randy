c4b0100d51fc15ef16292bbf1b7670c95d1d9a48
Samv <santiagoantoniomariscal@gmail.com>
2019-07-11 12:36:30 -0500

    samv

---
+++ ENSEÑANDO1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ENSEÑANDO1
+{
+
+    class calculador {
+
+        private int x { get; set; }
+        private int y { get; set; }
+        public void setValores(int xdsf,int ydsf) {
+            this.x = xdsf;
+            this.y = ydsf;
+        }
+
+
+        public int  sumar() {
+            return this.x + this.y;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insertar los valores");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+            calculador c = new calculador();
+            c.setValores(x,y);
+
+            int resultado = c.sumar();
+
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+           
+        }
+    }
+}
